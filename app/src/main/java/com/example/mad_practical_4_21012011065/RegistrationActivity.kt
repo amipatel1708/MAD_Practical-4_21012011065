3d2258e09239c9d502715a1b06a9ddacd75b6a25
student <student@gnu.ac.in>
2023-08-07 08:54:35 +0530

    Practical 4

--- app/src/main/java/com/example/mad_practical_4_21012011065/RegistrationActivity.kt
+++ app/src/main/java/com/example/mad_practical_4_21012011065/RegistrationActivity.kt
@@ -7,6 +7,5 @@ class RegistrationActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_registration)
-        val login:
     }
 }
