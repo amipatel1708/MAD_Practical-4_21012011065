e07bf29a6f703653d4906d0f05011c1d199a3a66
student <student@gnu.ac.in>
2023-08-10 08:56:57 +0530

    Practical - 4

--- app/src/main/java/com/example/mad_practical_4_21012011065/LoginActivity.kt
+++ app/src/main/java/com/example/mad_practical_4_21012011065/LoginActivity.kt
@@ -4,8 +4,6 @@ import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import android.widget.Button
-
-
 class LoginActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
--- app/src/main/java/com/example/mad_practical_4_21012011065/RegistrationActivity.kt
+++ app/src/main/java/com/example/mad_practical_4_21012011065/RegistrationActivity.kt
@@ -2,7 +2,6 @@ package com.example.mad_practical_4_21012011065
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
-
 class RegistrationActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
